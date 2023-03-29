a448951da65980a3aaf1d6595a5bf872d4e221ae
marcos.robles <mirh92@outlook.com>
2023-03-29 06:56:26 -0600

    InitialCreate

---
+++ Abstract/Abstract/MyBook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+
+    public abstract class Book
+    {
+        public abstract void display();
+        
+
+    }
+     class MyBook:Book
+    {
+        private readonly string _title;
+        private readonly string _author;
+        private readonly int _price;
+
+        public MyBook(string title, string author, int price)
+        {
+           _title = title;
+           _author = author;
+           _price = price;
+        }
+
+        public override void display()
+        {
+            Console.WriteLine($"Title: {_title}");
+            Console.WriteLine($"Auhtor: {_author}");
+            Console.WriteLine($"Price: {_price}");
+        }
+    }
+
+
+ 
+}
---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Abstract;
+
+Console.WriteLine("Hello, World!");
+Book _book = new MyBook("Ines del alma mia", "Isabel Allende", 300);
+_book.display();
